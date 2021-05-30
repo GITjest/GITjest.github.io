88780ea15ec123e3fee39decf46fb2634b8aeb47
GITjest <45946304+GITjest@users.noreply.github.com>
2021-05-30 19:02:36 +0200

    add menu

--- DO-calculate/js/App.js
+++ DO-calculate/js/App.js
@@ -18,6 +18,24 @@ $(function () {
     $("#ship-select-container").append(shipSelectGenerator.create(data.getShips(), ship));
     generator.generate(shipData);
 
+    $("#ship").on("change", function () {
+        $("#ship-container").css("display", "inline-block");
+        $("#skill-container").css("display", "none");
+        $("#statistic-detail-container").css("display", "none");
+    })
+
+    $("#skill-tree").on("change", function () {
+        $("#ship-container").css("display", "none");
+        $("#skill-container").css("display", "inline-block");
+        $("#statistic-detail-container").css("display", "none");
+    })
+
+    $("#statistic-details").on("change", function () {
+        $("#ship-container").css("display", "none");
+        $("#skill-container").css("display", "none");
+        $("#statistic-detail-container").css("display", "inline-block");
+    })
+
     // let json = JSON.stringify(ships, null, 1);
     // console.log(json);
 });
